023375e7a896560755ed96bed722a1e58157397a
Monica Björk <m.bjork95@gmail.com>
2018-05-21 10:00:18 +0200

    User can now add numbers

--- EnVektor/Program.cs
+++ EnVektor/Program.cs
@@ -8,6 +8,27 @@ namespace EnVektor {
     class Program {
         static void Main(string[] args) {
             int[] someNumbers = new int[10];
+            bool validData = true;
+
+            for(int i = 0; i < 10; i++) {
+                do {
+                    Console.Write("Skriv ett tal: ");
+                    try {
+                        someNumbers[i] = Convert.ToInt32(Console.ReadLine());
+                        validData = true;
+                    }
+                    catch(FormatException) {
+                        Console.WriteLine("Var snäll och skriv endast heltal");
+                        validData = false;
+                    }
+                    catch(Exception ex) {
+                        Console.WriteLine(ex.Message);
+                        validData = false;
+                    }
+                } while(validData == false);
+            }
+            Console.WriteLine("Tack!");
+            Console.ReadKey();
         }
     }
 }
